84d3be60fa5cd3d34b85455442a7811fc948220a
AnimeKaizoku <animekaizokux@gmail.com>
2020-07-16 10:55:05 +0530

    github still being gay with ignores

--- config.py
+++
@@ -1,8 +0,0 @@
-# Your Bot token in the quotes taken from @BotFather
-BOT_TOKEN = "311423291624:sihtrofmiaotyagyltenifederauoyfolla"
-
-# Project Name of your Project
-PROJECT_NAME = "SaitamaRobot"
-
-# If you forked the source code, grab your repo's link and paste it below!
-GIT_REPO_URL = "https://github.com/AnimeKaizoku/SaitamaRobot"

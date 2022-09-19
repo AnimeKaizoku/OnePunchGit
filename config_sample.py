9675e790b9c6942f528b0012d88f990c9ce86a50
Droid Knight <62333331+DroidMeUp@users.noreply.github.com>
2022-09-20 00:55:04 +0530

    DELETE THIS TRASH NOOB BOT!! #SHAMEONYOUNOOB

--- config_sample.py
+++ config_sample.py
@@ -1,10 +1,1 @@
-# Open port in firewall by running the following cmd command. 
-# netsh advfirewall firewall add rule name="One Punch Git" dir=in action=allow protocol=TCP localport=1258
-# Your Bot token in the quotes taken from @BotFather
-BOT_TOKEN = ""
 
-# Project Name of your Project
-PROJECT_NAME = ""
-
-# If you forked the source code, grab your repo's link and paste it below!
-GIT_REPO_URL = "https://github.com/MadeByThePinsHub/GitGram"
